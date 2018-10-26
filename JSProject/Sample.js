faede381c78a04beda5cb0fcd386218b054b477e
maniyo <Manish.Bajaj@dieboldnixdorf.com>
2018-10-26 12:07:35 +0530

    New commit

---
+++ JSProject/Sample.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+function validate()
+{
+      var inputs = new Array();
+      inputs[0] = document.getElementById('a').value;
+      inputs[1] = document.getElementById('b').value;
+      inputs[2] = document.getElementById('c').value;
+      inputs[3] = document.getElementById('d').value;
+      for (i in inputs)
+      {
+        if (inputs[i] == "")
+        	document.getElementById(div).innerHTML = "Error";
+        else
+        {
+          var a = document.getElementById('a').value;
+          var b = document.getElementById('b').value;
+        }
+       }
+ }
